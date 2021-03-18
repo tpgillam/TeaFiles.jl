bba1063984ecd0947ee0a385e3d7bbcf7e37bc44
Documenter.jl <documenter@juliadocs.github.io>
2021-03-18 11:06:44 +0000

    build based on 7f6fb15

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TeaFiles","category":"page"},{"location":"#TeaFiles","page":"Home","title":"TeaFiles","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TeaFiles]","category":"page"}]
+}
