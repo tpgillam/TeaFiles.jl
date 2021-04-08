ca1bc181d8b74fb042c27fbfe25023bb0f22ffb0
Thomas Gillam <tpgillam@googlemail.com>
2021-04-08 15:39:14 +0100

    Move function to more logical place

--- src/header/core.jl
+++ src/header/core.jl
@@ -14,89 +14,6 @@ abstract type AbstractSection end
     sections::Vector{AbstractSection}
 end
 
-"""Throw if `metadata` is invalid."""
-function verify_metadata(metadata::TeaFileMetadata)::Nothing
-    if metadata.item_start % 8 != 0
-        throw(ArgumentError(
-            "Invalid item_start: $(metadata.item_start) is not a multiple of 8."
-        ))
-    end
-
-    if metadata.item_end != 0 && metadata.item_end < metadata.item_start
-        throw(ArgumentError(
-            "Invalid item_end: $(metadata.item_end) is before item_start."
-        ))
-    end
-
-    # We should have at most one section of any type.
-    type_to_section = Dict{Type, AbstractSection}()
-    type_to_count = DefaultDict{Type, Int}(0)
-    for section in metadata.sections
-        type_to_count[typeof(section)] += 1
-        type_to_section[typeof(section)] = section
-    end
-    for (type, count) in type_to_count
-        if count > 1
-            throw(ArgumentError(
-                "Found $count sections of type $type -- there should be at most one."
-            ))
-        end
-    end
-
-    # At this point we could have zero or one time section.
-    if haskey(type_to_section, TimeSection)
-        # If the time section is present, we will do some tests
-        time_section::TimeSection = type_to_section[TimeSection]
-
-        # We *must* have an item section if we have a time section.
-        if !haskey(type_to_section, ItemSection)
-            throw(ArgumentError("There is a TimeSection, but no ItemSection."))
-        end
-        item_section = type_to_section[ItemSection]
-
-        unclaimed_field_offsets = Set((field.offset for field in item_section.fields))
-        # Verify that time field offsets match up to known fields, and that each field is
-        # referred to exactly once.
-        for time_field_offset in time_section.time_field_offsets
-            if !(time_field_offset in unclaimed_field_offsets)
-                throw(ArgumentError(
-                    "Time field offset $time_field_offset does not match to a field."
-                ))
-            end
-
-            # We have claimed this field.
-            delete!(unclaimed_field_offsets, time_field_offset)
-        end
-    end
-
-    if haskey(type_to_section, ItemSection)
-        item_section::ItemSection = type_to_section[ItemSection]
-
-        offsets = [field.offset for field in item_section.fields]
-        # `item_size` takes us to the start of the next item.
-        push!(offsets, item_section.item_size)
-        actual_field_sizes = diff(offsets)
-
-        minimum_field_sizes = [sizeof(field_type(field)) for field in item_section.fields]
-
-        for (i, (actual, minimum)) in enumerate(
-                zip(actual_field_sizes, minimum_field_sizes))
-            # This verifies that all item offsets are strictly increasing, and each field
-            # has at least enough space on disk to store the type.
-            if actual < minimum
-                throw(ArgumentError(
-                    "offsets imply field $i has size $actual, "
-                    * "but it should be at least $minimum"
-                ))
-            end
-        end
-    end
-
-    # TODO Verify that item_start is large enough to be after the whole metadata section.
-
-    return nothing
-end
-
 const _FIELD_DATA_TYPE_TO_ID = Bijection(Dict{DataType,Int32}(
     Int8 => 1,
     Int16 => 2,
@@ -233,4 +150,87 @@ function is_item_compatible(Item::Type, item_section::ItemSection)::Bool
     end
 
     return true
+end
+
+"""Throw if `metadata` is invalid."""
+function verify_metadata(metadata::TeaFileMetadata)::Nothing
+    if metadata.item_start % 8 != 0
+        throw(ArgumentError(
+            "Invalid item_start: $(metadata.item_start) is not a multiple of 8."
+        ))
+    end
+
+    if metadata.item_end != 0 && metadata.item_end < metadata.item_start
+        throw(ArgumentError(
+            "Invalid item_end: $(metadata.item_end) is before item_start."
+        ))
+    end
+
+    # We should have at most one section of any type.
+    type_to_section = Dict{Type, AbstractSection}()
+    type_to_count = DefaultDict{Type, Int}(0)
+    for section in metadata.sections
+        type_to_count[typeof(section)] += 1
+        type_to_section[typeof(section)] = section
+    end
+    for (type, count) in type_to_count
+        if count > 1
+            throw(ArgumentError(
+                "Found $count sections of type $type -- there should be at most one."
+            ))
+        end
+    end
+
+    # At this point we could have zero or one time section.
+    if haskey(type_to_section, TimeSection)
+        # If the time section is present, we will do some tests
+        time_section::TimeSection = type_to_section[TimeSection]
+
+        # We *must* have an item section if we have a time section.
+        if !haskey(type_to_section, ItemSection)
+            throw(ArgumentError("There is a TimeSection, but no ItemSection."))
+        end
+        item_section = type_to_section[ItemSection]
+
+        unclaimed_field_offsets = Set((field.offset for field in item_section.fields))
+        # Verify that time field offsets match up to known fields, and that each field is
+        # referred to exactly once.
+        for time_field_offset in time_section.time_field_offsets
+            if !(time_field_offset in unclaimed_field_offsets)
+                throw(ArgumentError(
+                    "Time field offset $time_field_offset does not match to a field."
+                ))
+            end
+
+            # We have claimed this field.
+            delete!(unclaimed_field_offsets, time_field_offset)
+        end
+    end
+
+    if haskey(type_to_section, ItemSection)
+        item_section::ItemSection = type_to_section[ItemSection]
+
+        offsets = [field.offset for field in item_section.fields]
+        # `item_size` takes us to the start of the next item.
+        push!(offsets, item_section.item_size)
+        actual_field_sizes = diff(offsets)
+
+        minimum_field_sizes = [sizeof(field_type(field)) for field in item_section.fields]
+
+        for (i, (actual, minimum)) in enumerate(
+                zip(actual_field_sizes, minimum_field_sizes))
+            # This verifies that all item offsets are strictly increasing, and each field
+            # has at least enough space on disk to store the type.
+            if actual < minimum
+                throw(ArgumentError(
+                    "offsets imply field $i has size $actual, "
+                    * "but it should be at least $minimum"
+                ))
+            end
+        end
+    end
+
+    # TODO Verify that item_start is large enough to be after the whole metadata section.
+
+    return nothing
 end
